91ab4e37c1df30976b32cb456700c3f2a0dbf702
Cole Thomson <cthomson4@wisc.edu>
2020-02-14 16:22:52 -0600

    CS 407: Lab 3 Milestone 1 (small adj to menu)

--- app/src/main/java/com/example/lab3milestone1/MainActivity.java
+++ app/src/main/java/com/example/lab3milestone1/MainActivity.java
@@ -65,7 +65,7 @@ public class MainActivity extends AppCompatActivity {
 
             default:
                 return super.onOptionsItemSelected(item);
-                    
+
         }
     }
 }
